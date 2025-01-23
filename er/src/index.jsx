76c1720d49a9e62dbc03533d94e5107732d84fc7
TheDreamFactoryNZ <122509590+TheDreamFactoryNZ@users.noreply.github.com>
2025-01-23 17:41:41 +1100

    Add React.StrictMode wrapper to App.jsx for development checks, clean up component structure

--- er/src/index.jsx
+++ er/src/index.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
 import App from './App';
-
 // import config from
 
-render(<App configFile='./public/config/config.json' />, document.getElementById('root'));
-// render(<App configFile={'./config/config.json'} />, document.getElementById('root'))
+render(
+  <StrictMode>
+    <App configFile='./public/config/config.json' />
+  </StrictMode>,
+  document.getElementById('root')
+);
