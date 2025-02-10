51b0758ae8c97e81a143bd38f3b8453cefbbc5c8
TheDreamFactoryNZ <122509590+TheDreamFactoryNZ@users.noreply.github.com>
2025-02-11 01:54:39 +1100

    Configuration of development environment including addition of webpack, webpack dev server, copy-webpack-plugin and html-webpack-plugin. Addition of babel for compiling

---
+++ er2/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isDev = argv.mode === "development";
+
+  return {
+    entry: "./src/index.jsx",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js",
+      clean: true, // Cleans the output directory before building
+    },
+    mode: isDev ? "development" : "production",
+    devtool: isDev ? "inline-source-map" : "source-map",
+    devServer: {
+      static: path.join(__dirname, "public"),
+      hot: true,
+      open: true,
+      port: 3000,
+      historyApiFallback: true, // Ensures React Router works
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|webp|svg)$/,
+          type: "asset/resource",
+          generator: {
+            filename: "assets/[name][ext]",
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "public"),
+            to: path.resolve(__dirname, "build", "public"),
+            globOptions: {
+              ignore: ["**/index.html"], // Prevents overwriting HtmlWebpackPlugin
+            },
+          },
+        ],
+      }),
+    ],
+  };
+};
