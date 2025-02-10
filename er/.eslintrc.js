c9c58e2f9a90ecb16632ee32ee4c585cc123d81f
TheDreamFactoryNZ <122509590+TheDreamFactoryNZ@users.noreply.github.com>
2025-02-10 23:34:43 +1100

    Disable the no longer needed react/jsx-uses-react and react/react-in-jsx-scope rules

--- er/.eslintrc.js
+++ er/.eslintrc.js
@@ -1,3 +1,10 @@
 const neutrino = require('neutrino');
 
-module.exports = neutrino().eslintrc();
+module.exports = {
+    extends: [require('@neutrinojs/react').eslintrc()],
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off'
+    }
+  };
+  
