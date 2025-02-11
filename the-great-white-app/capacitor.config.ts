3b4fcc7b69120ba1bee062448be1d70ef991d477
TheDreamFactoryNZ <122509590+TheDreamFactoryNZ@users.noreply.github.com>
2025-02-11 19:48:42 +1100

    Initialise Ionic app

---
+++ the-great-white-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'The Great White App',
+  webDir: 'dist'
+};
+
+export default config;
