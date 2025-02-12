c168db69ab1712ea387f977ce0dc6d1b63d5db5f
TheDreamFactoryNZ <122509590+TheDreamFactoryNZ@users.noreply.github.com>
2025-02-12 15:15:55 +1100

    Manually specify babel.config.js path

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -27,6 +27,9 @@ module.exports = (env, argv) => {
           exclude: /node_modules/,
           use: {
             loader: "babel-loader",
+            options: {
+              configFile: path.resolve(__dirname, "babel.config.js"),
+            },
           },
         },
         {
