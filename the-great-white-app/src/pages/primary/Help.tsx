d1f86e0f5f69b1fa12f5870b7869656120774aea
TheDreamFactoryNZ <122509590+TheDreamFactoryNZ@users.noreply.github.com>
2025-02-13 18:51:02 +1100

    Design improvements

--- the-great-white-app/src/pages/primary/Help.tsx
+++ the-great-white-app/src/pages/primary/Help.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonItem, IonLabel, IonPage, IonText, IonGrid, IonCol, IonRow, IonTitle, IonToolbar, IonCard, IonIcon, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/react';
-import { informationCircleOutline, happyOutline, helpCircleOutline, bugOutline, fishOutline } from 'ionicons/icons'
+import { IonContent, IonHeader, IonPage, IonGrid, IonCol, IonRow, IonTitle, IonToolbar, IonCard, IonIcon, IonCardHeader, IonCardTitle, IonCardSubtitle, IonImg } from '@ionic/react';
+import { informationCircleOutline, happyOutline, helpCircleOutline, bugOutline, fishOutline, arrowForwardCircleOutline } from 'ionicons/icons'
 import './Help.css';
 
 const Help: React.FC = () => {
@@ -14,48 +14,53 @@ const Help: React.FC = () => {
         <IonGrid fixed={true}>
           <IonRow>
             <IonCol size="12" size-xs="12" size-sm="12" size-md="6" size-lg="6" size-xl="6">
-              <IonCard>
+              <IonCard className='help-card'>
                 <IonIcon aria-hidden="true" className='help-icon' icon={informationCircleOutline} color="primary"></IonIcon>
-                <IonCardHeader>
+                <IonCardHeader className='help-card-content'>
                   <IonCardTitle>About the Great White Project</IonCardTitle>
                   <IonCardSubtitle>About our mission to understand Great Whites around NZ.</IonCardSubtitle>
+                    <IonIcon size="large" className='help-card-arrow' aria-label="Open information" icon={arrowForwardCircleOutline}></IonIcon>
                 </IonCardHeader>
               </IonCard>
             </IonCol>
             <IonCol size="12" size-xs="12" size-sm="12" size-md="6" size-lg="6" size-xl="6">
-              <IonCard>
+              <IonCard className='help-card'>
                 <IonIcon aria-hidden="true" className='help-icon' icon={happyOutline} color="primary"></IonIcon>
-                <IonCardHeader>
+                <IonCardHeader className='help-card-content'>
                   <IonCardTitle>Using the Great White App</IonCardTitle>
                   <IonCardSubtitle>Information on interacting with the map and sharks.</IonCardSubtitle>
+                    <IonIcon size="large" className='help-card-arrow' aria-label="Open information" icon={arrowForwardCircleOutline}></IonIcon>
                 </IonCardHeader>
               </IonCard>
             </IonCol>
             <IonCol size="12" size-xs="12" size-sm="12" size-md="6" size-lg="6" size-xl="6">
-              <IonCard>
+              <IonCard className='help-card'>
                 <IonIcon aria-hidden="true" className='help-icon' icon={helpCircleOutline} color="primary"></IonIcon>
-                <IonCardHeader>
+                <IonCardHeader className='help-card-content'>
                   <IonCardTitle>Common Questions</IonCardTitle>
                   <IonCardSubtitle>FAQ about the project, app and sharks.</IonCardSubtitle>
+                    <IonIcon size="large" className='help-card-arrow' aria-label="Open information" icon={arrowForwardCircleOutline}></IonIcon>
                 </IonCardHeader>
               </IonCard>
             </IonCol>
             <IonCol size="12" size-xs="12" size-sm="12" size-md="6" size-lg="6" size-xl="6">
-              <IonCard>
+              <IonCard className='help-card'>
                 <IonIcon aria-hidden="true" className='help-icon' icon={bugOutline} color="primary"></IonIcon>
-                <IonCardHeader>
+                <IonCardHeader className='help-card-content'>
                   <IonCardTitle>Report an Issue</IonCardTitle>
                   <IonCardSubtitle>Found a bug? Something not working? Let us know.</IonCardSubtitle>
+                    <IonIcon size="large" className='help-card-arrow' aria-label="Open information" icon={arrowForwardCircleOutline}></IonIcon>
                 </IonCardHeader>
               </IonCard>
             </IonCol>
             <IonCol size="12" size-xs="12" size-sm="12" size-md="6" size-lg="6" size-xl="6">
-              <a href="https://www.sustainableoceansociety.co.nz/" target="_blank">
-                <IonCard>
+              <a className='help-card-link' href="https://www.sustainableoceansociety.co.nz/" target="_blank">
+                <IonCard className='help-card'>
                   <IonIcon aria-hidden="true" className='help-icon' icon={fishOutline} color="primary"></IonIcon>
-                  <IonCardHeader>
+                  <IonCardHeader className='help-card-content'>
                     <IonCardTitle>About Sustainable Oceans Society</IonCardTitle>
-                    <IonCardSubtitle>Visit our website and learn more about us.</IonCardSubtitle>
+                    <IonCardSubtitle>Learn more about us and visit our website</IonCardSubtitle>
+                    <IonIcon size="large" className='help-card-arrow' aria-label="Open link to website" icon={arrowForwardCircleOutline}></IonIcon>
                   </IonCardHeader>
                 </IonCard>
               </a>
