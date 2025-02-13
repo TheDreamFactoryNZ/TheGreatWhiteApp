1b793b498eb7272f381f33a473eef5c41ed57149
TheDreamFactoryNZ <122509590+TheDreamFactoryNZ@users.noreply.github.com>
2025-02-13 17:34:40 +1100

    Adjustments to icon style

--- the-great-white-app/src/pages/primary/Help.tsx
+++ the-great-white-app/src/pages/primary/Help.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonHeader, IonItem, IonLabel, IonPage, IonText, IonGrid, IonCol, IonRow, IonTitle, IonToolbar, IonCard, IonIcon, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/react';
-import { informationCircleOutline, navigateOutline, helpCircleOutline, bugOutline, fishOutline } from 'ionicons/icons'
+import { informationCircleOutline, happyOutline, helpCircleOutline, bugOutline, fishOutline } from 'ionicons/icons'
 import './Help.css';
 
 const Help: React.FC = () => {
@@ -15,7 +15,7 @@ const Help: React.FC = () => {
           <IonRow>
             <IonCol size="12" size-xs="12" size-sm="12" size-md="6" size-lg="6" size-xl="6">
               <IonCard>
-                <IonIcon icon={informationCircleOutline} size="large" color="primary"></IonIcon>
+                <IonIcon aria-hidden="true" className='help-icon' icon={informationCircleOutline} color="primary"></IonIcon>
                 <IonCardHeader>
                   <IonCardTitle>About the Great White Project</IonCardTitle>
                   <IonCardSubtitle>About our mission to understand Great Whites around NZ.</IonCardSubtitle>
@@ -24,7 +24,7 @@ const Help: React.FC = () => {
             </IonCol>
             <IonCol size="12" size-xs="12" size-sm="12" size-md="6" size-lg="6" size-xl="6">
               <IonCard>
-                <IonIcon icon={navigateOutline} size="large" color="primary"></IonIcon>
+                <IonIcon aria-hidden="true" className='help-icon' icon={happyOutline} color="primary"></IonIcon>
                 <IonCardHeader>
                   <IonCardTitle>Using the Great White App</IonCardTitle>
                   <IonCardSubtitle>Information on interacting with the map and sharks.</IonCardSubtitle>
@@ -33,7 +33,7 @@ const Help: React.FC = () => {
             </IonCol>
             <IonCol size="12" size-xs="12" size-sm="12" size-md="6" size-lg="6" size-xl="6">
               <IonCard>
-                <IonIcon icon={helpCircleOutline} size="large" color="primary"></IonIcon>
+                <IonIcon aria-hidden="true" className='help-icon' icon={helpCircleOutline} color="primary"></IonIcon>
                 <IonCardHeader>
                   <IonCardTitle>Common Questions</IonCardTitle>
                   <IonCardSubtitle>FAQ about the project, app and sharks.</IonCardSubtitle>
@@ -42,7 +42,7 @@ const Help: React.FC = () => {
             </IonCol>
             <IonCol size="12" size-xs="12" size-sm="12" size-md="6" size-lg="6" size-xl="6">
               <IonCard>
-                <IonIcon icon={bugOutline} size="large" color="primary"></IonIcon>
+                <IonIcon aria-hidden="true" className='help-icon' icon={bugOutline} color="primary"></IonIcon>
                 <IonCardHeader>
                   <IonCardTitle>Report an Issue</IonCardTitle>
                   <IonCardSubtitle>Found a bug? Something not working? Let us know.</IonCardSubtitle>
@@ -52,7 +52,7 @@ const Help: React.FC = () => {
             <IonCol size="12" size-xs="12" size-sm="12" size-md="6" size-lg="6" size-xl="6">
               <a href="https://www.sustainableoceansociety.co.nz/" target="_blank">
                 <IonCard>
-                  <IonIcon icon={fishOutline} size="large" color="primary"></IonIcon>
+                  <IonIcon aria-hidden="true" className='help-icon' icon={fishOutline} color="primary"></IonIcon>
                   <IonCardHeader>
                     <IonCardTitle>About Sustainable Oceans Society</IonCardTitle>
                     <IonCardSubtitle>Visit our website and learn more about us.</IonCardSubtitle>
