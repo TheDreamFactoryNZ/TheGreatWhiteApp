903007c209c647a47c7efce1e9dbfa11d22eb400
TheDreamFactoryNZ <122509590+TheDreamFactoryNZ@users.noreply.github.com>
2025-03-06 22:42:45 +1100

    Add fallback image while map is loading

--- the-great-white-app/src/pages/primary/SharkMap.tsx
+++ the-great-white-app/src/pages/primary/SharkMap.tsx
@@ -9,6 +9,9 @@ const SharkMap: React.FC = () => {
     <IonPage>
       <IonContent fullscreen>
         <StrictMode>
+          <div id="map-image-container">
+          <img id="map-image" src="https://api.mapbox.com/styles/v1/izaakwicks/cm695163r00h001spfqn33x5h/static/173.4961,-40.0444,5,0/1280x1280?access_token=pk.eyJ1IjoidmpvZWxtIiwiYSI6ImNra2hiZXNpMzA1bTcybnA3OXlycnN2ZjcifQ.gH6Nls61WTMVutUH57jMJQ" />
+          </div>
           <div id='map-page'>
             <MainApp configFile='https://map.sustainableoceansociety.co.nz/public/config/config.json' />
           </div>
