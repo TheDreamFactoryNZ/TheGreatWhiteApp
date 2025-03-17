7bbf1aa85d4bb4a31bc0439728957ffc27009782
Izaak Wicks <izaak.wicks@thedreamfactory.nz>
2025-03-17 19:36:34 +1100

    Install and configure Splash Screen plugin

--- the-great-white-app/capacitor.config.ts
+++ the-great-white-app/capacitor.config.ts
@@ -10,6 +10,19 @@ const config: CapacitorConfig = {
       style: "DEFAULT",
       backgroundColor: "#FFFFFFFF",
     },
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#101855",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
   },
 };
 
