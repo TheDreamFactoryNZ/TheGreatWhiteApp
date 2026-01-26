606c293ccef1873df022c5c0c602663c61a41374
TheDreamFactoryNZ <122509590+TheDreamFactoryNZ@users.noreply.github.com>
2026-01-27 00:47:47 +1100

    Add core folder as a path alias to simplify Sharkmap.tsx

--- the-great-white-app/src/pages/primary/SharkMap.tsx
+++ the-great-white-app/src/pages/primary/SharkMap.tsx
@@ -1,8 +1,8 @@
 import { IonContent, IonPage } from '@ionic/react';
 import './SharkMap.css';
-import '../../../../core/assets/mapstyle.css';
+import '@core/assets/mapstyle.css';
 import React, { StrictMode } from 'react';
-import MainApp from '../../../../core/App';
+import MainApp from '@core/App';
 
 const SharkMap: React.FC = () => {
   return (
