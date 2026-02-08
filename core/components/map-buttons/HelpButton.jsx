f995c366799e2bdab14bd2e96f066985e23f514e
TheDreamFactoryNZ <122509590+TheDreamFactoryNZ@users.noreply.github.com>
2026-02-08 14:02:58 +1100

    Style and amend HelpButton text

--- core/components/map-buttons/HelpButton.jsx
+++ core/components/map-buttons/HelpButton.jsx
@@ -72,51 +72,51 @@ const HelpButton = () => {
         <div className={styles.tipItemsContainer}>
           <div className={styles.tipItem}>
             <div className={`${styles.tipIconContainer} ${styles['tipIconVertical']}`}><img className={styles.tipIcon} width='20' height='40' src={zoom} /></div>
-            <div className={styles.tipTextContainer}><p>Zoom in and out with the + and - buttons.</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body">Zoom in and out with the + and - buttons.</p></div>
           </div>
           <div className={styles.tipItem}>
             <div className={styles.tipIconContainer}><img className={styles.tipIcon} width='24' height='24' src={reset} /></div>
-            <div className={styles.tipTextContainer}><p>Press to reset map orientation, hold and drag to adjust pitch.</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body">Press to reset map orientation, hold and drag to adjust pitch.</p></div>
           </div>
           <div className={`${styles.tipItem} desktopItem`}>
             <div className={styles.tipIconContainer}><img className={styles.tipIcon} width='24' height='24' src={control} /></div>
-            <div className={styles.tipTextContainer}><p>Hold Ctrl / control key and drag left to right with mouse to rotate view, up and down to adjust pitch.</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body">Hold Ctrl / control key and drag left to right with mouse to rotate view, up and down to adjust pitch.</p></div>
           </div>
           <div className={styles.tipItem}>
             <div className={styles.tipIconContainer}><img className={styles.tipIcon} width='24' height='24' src={animalIcon} /></div>
-            <div className={styles.tipTextContainer}><p>Choose a shark icon on the map to view summary and date of last known location.</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body">Choose a shark icon on the map to view summary and date of last known location.</p></div>
           </div>
           <div className={styles.tipItem}>
             <div className={styles.tipIconContainer}><img className={styles.tipIcon} width='24' height='24' src={activeAnimalIcon} /></div>
-            <div className={styles.tipTextContainer}><p>A green light means the tag on the shark is active and transmitting location data.</p><p><strong>No recent updates?</strong> This is because the shark hasn't surfaced high enough to transmit a location to satellites.</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body">A green light means the tag on the shark is active and transmitting location data.</p><p className="map-body"><strong>No recent updates?</strong> This is because the shark hasn't surfaced high enough to transmit a location to satellites.</p></div>
           </div>
           {/* vvv This tip will be restored when automatic status updates are implemented vvv */}
           <div className={styles.tipItem} style={{ display: 'none' }}>
             <div className={styles.tipIconContainer}><img className={styles.tipIcon} width='24' height='24' src={inactiveAnimalIcon} /></div>
-            <div className={styles.tipTextContainer}><p>These sharks have not transmitted a location for an extended period of time and are inactive.</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body">These sharks have not transmitted a location for an extended period of time and are inactive.</p></div>
           </div>
           <div className={styles.tipItem}>
             <div className={styles.tipIconContainer}><img className={styles.tipIcon} width='24' height='24' src={deactivatedAnimalIcon} /></div>
-            <div className={styles.tipTextContainer}><p>These sharks have lost their tags and are confirmed no longer transmitting.</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body">These sharks have lost their tags and are confirmed no longer transmitting.</p></div>
           </div>
           <div className={styles.tipItem}>
             <div className={styles.tipIconContainer}><img className={styles.tipIcon} width='24' height='24' src={pin} /></div>
-            <div className={styles.tipTextContainer}><p>Jump to a shark's location.</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body">Jump to a shark's location.</p></div>
           </div>
           <div className={styles.tipItem}>
             <div className={styles.tipIconContainer}><img className={styles.tipIcon} width='24' height='24' src={tracks} /></div>
-            <div className={styles.tipTextContainer}><p>Display a shark's track.</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body">Display a shark's track.</p></div>
           </div>
           <div className={styles.tipItem}>
             <div className={styles.tipIconContainer}><img className={styles.tipIcon} width='7' height='10' src={story} /></div>
-            <div className={styles.tipTextContainer}><p>Display a shark's story.</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body">Display a shark's story.</p></div>
           </div>
           <div className={styles.tipItem}>
             <div className={styles.tipIconContainer}><img className={styles.tipIcon} width='24' height='24' src={refresh} /></div>
-            <div className={styles.tipTextContainer}><p>Refresh the map and fetch new locations (internet connection required)</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body">Refresh the map and fetch new locations (internet connection required)</p></div>
           </div>
           <div className={styles.tipItem}>
-            <div className={styles.tipTextContainer}><p><strong>Why hasn't there been an update?</strong> A shark needs to breach the surface of the ocean to transmit its location to satellites.</p><p>These majestic creatures can spend <em><strong>months</strong></em> underwater, so it is not unusual for there to be periods of inactivity.</p></div>
+            <div className={styles.tipTextContainer}><p className="map-body"><strong>Why hasn't there been an update?</strong> A shark needs to breach the surface of the ocean to transmit its location to satellites.</p><p className="map-body">These majestic creatures can spend <em><strong>months</strong></em> underwater, so it is not unusual for there to be periods of inactivity.</p></div>
           </div>
         </div>
       </div>
