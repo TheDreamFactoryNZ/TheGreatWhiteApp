b9ffe11ae1d87dad2db947fd7a4260dbcfa084d7
TheDreamFactoryNZ <122509590+TheDreamFactoryNZ@users.noreply.github.com>
2026-03-08 17:09:10 +1100

    feat: use bootstrap hook for config loading on web and mobile app

--- core/index.jsx
+++ core/index.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { useBootstrapConfig } from "@hooks/useBootstrapConfig.js";
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App configFile='https://map.sustainableoceansociety.co.nz/public/config/config.json' />
-  </React.StrictMode>
-);
+function Root() {
+  const configUrl = useBootstrapConfig();
+
+  if (!configUrl) {
+    return null;
+  }
+
+  return (
+    <StrictMode>
+      <App configFile={configUrl} />
+    </StrictMode>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Root />);
--- mobileapp/src/pages/primary/SharkMap.tsx
+++ mobileapp/src/pages/primary/SharkMap.tsx
@@ -1,15 +1,22 @@
 import { IonContent, IonPage } from '@ionic/react';
 import React, { StrictMode } from 'react';
 import MainApp from '@core/App';
+import { useBootstrapConfig } from '@hooks/useBootstrapConfig.js';
 import './SharkMap.css';
 
 const SharkMap: React.FC = () => {
+  const configUrl = useBootstrapConfig();
+
+  if (!configUrl) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <IonPage>
       <IonContent fullscreen>
         <StrictMode>
           <div id='map-page'>
-            <MainApp configFile='https://map.sustainableoceansociety.co.nz/public/config/config.json' />
+            <MainApp configFile={configUrl} />
           </div>
         </StrictMode>
       </IonContent>
